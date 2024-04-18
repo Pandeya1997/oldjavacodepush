eb15fd3ff8f05180981dca951e327818f9d6dcc4
Pandeya1997 <abhishekpy55@gmail.com>
2024-04-18 08:20:40 +0530

    pabhi

---
+++ chw_27_array_for_each_loop.java
@@ -0,0 +1,36 @@
+public class chw_27_array_for_each_loop {
+    public static void main(String[] args) {
+      /*
+        int [] marks =  {70,80,98,45,56};
+        System.out.println("array fourt possitions number ");
+        System.out.println(marks[3]);
+        System.out.println("array length");
+        System.out.println(marks.length);
+        System.out.println("array traveling");
+        for(int i = 0; i<marks.length ; i++){
+            System.out.println(marks[i]);
+        }
+        float [] marks1 = {10.5f,25.7f,88.5f,78.5f};
+        System.out.println("array fourt possitions number floating ");
+        System.out.println(marks1[3]);
+        System.out.println("array length float");
+        System.out.println(marks1.length);
+        System.out.println("array travling floating");
+        for(int p = 0; p<=marks1.length; p++){
+            System.out.println(marks1[p]);
+        }
+
+       */
+        String [] name = {"Abhishek","Ashish","Ashutosh","Vishal","Vikash"};
+        System.out.println("String name length");
+        System.out.println(name.length);
+        System.out.println(name[2]);
+
+        //for(String s = 0; s<=String.(name-1);s++){
+           // System.out.println(name[s]);
+        //}
+
+
+
+    }
+}
