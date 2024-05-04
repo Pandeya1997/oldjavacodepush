746fb46fe46899250ff3a790c94535a8a19ca5a4
Pandeya1997 <abhishekpy55@gmail.com>
2024-05-04 23:06:15 +0530

    Merge branch 'main' of https://github.com/Pandeya1997/oldjavacodepush "static mathod"

---
+++ chw_32_void_return_methods.java
@@ -0,0 +1,8 @@
+public class chw_32_void_return_methods {
+    public static void main(String[] args) {
+        telljoks();
+    }
+   static  void telljoks (){
+        System.out.println("टीचर- इतने दिन कहां थे, स्कूल क्यों नहीं आए? पप्पू का जवाब सुनकर नहीं रोक पाएंगे हंसी");
+    }
+}
